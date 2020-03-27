630662f38061b594b8ef3d359853df976ba1d73a
wdj3488 <wdj1218@126.com>
2020-03-27 13:34:41 +0800

    生成仿真文件

---
+++ CalDataIn.m
@@ -0,0 +1,116 @@
+% load('exp1_1021.mat');
+% A  =  exp1_1021;
+% fid = fopen('exp1_1021.txt','wt');
+% for i = 1:1:length(A.Y(970).Data)
+%     if A.Y(967).Data(i) < 0
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+360)*10);  
+%     else
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+0)*10);  
+%     end
+% end
+
+% load('exp1_1022.mat');
+% A  =  exp1_1022;
+% fid = fopen('exp1_1022.txt','wt');
+% for i = 1:1:length(A.Y(970).Data)
+%     if A.Y(967).Data(i) < 0
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+360)*10);  
+%     else
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+0)*10);  
+%     end
+% end
+
+
+% load('exp1_1027.mat');
+% A  =  exp1_1027;
+% fid = fopen('exp1_1027.txt','wt');
+% for i = 1:1:length(A.Y(970).Data)
+%     if A.Y(967).Data(i) < 0
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+360)*10);  
+%     else
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+0)*10);  
+%     end
+% end
+
+
+% load('exp1_1029.mat');
+% A  =  exp1_1029;
+% fid = fopen('exp1_1029.txt','wt');
+% for i = 1:1:length(A.Y(970).Data)
+%     if A.Y(967).Data(i) < 0
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+360)*10);  
+%     else
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+0)*10);  
+%     end
+% end
+
+% 
+% load('exp1_1032.mat');
+% A  =  exp1_1032;
+% fid = fopen('exp1_1032.txt','wt');
+% for i = 1:1:length(A.Y(970).Data)
+%     if A.Y(967).Data(i) < 0
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+360)*10);  
+%     else
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+0)*10);  
+%     end
+% end
+
+
+% load('exp1_1015.mat');
+% A  =  exp1_1015;
+% fid = fopen('exp1_1015.txt','wt');
+% for i = 1:1:length(A.Y(970).Data)
+%     if A.Y(967).Data(i) < 0
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+360)*10);  
+%     else
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+0)*10);  
+%     end
+% end
+
+% 
+% load('exp1_1014.mat');
+% A  =  exp1_1014;
+% fid = fopen('exp1_1014.txt','wt');
+% for i = 1:1:length(A.Y(970).Data)
+%     if A.Y(967).Data(i) < 0
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+360)*10);  
+%     else
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+0)*10);  
+%     end
+% end
+
+
+% load('exp1_1025.mat');
+% A  =  exp1_1025;
+% fid = fopen('exp1_1025.txt','wt');
+% for i = 1:1:length(A.Y(970).Data)
+%     if A.Y(967).Data(i) < 0
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+360)*10);  
+%     else
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+0)*10);  
+%     end
+% end
+
+
+% load('exp1_1028.mat');
+% A  =  exp1_1028;
+% fid = fopen('exp1_1028.txt','wt');
+% for i = 1:1:length(A.Y(970).Data)
+%     if A.Y(967).Data(i) < 0
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+360)*10);  
+%     else
+%         fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+0)*10);  
+%     end
+% end
+
+load('exp1_1031.mat');
+A  =  exp1_1031;
+fid = fopen('exp1_1031.txt','wt');
+for i = 1:1:length(A.Y(970).Data)
+    if A.Y(967).Data(i) < 0
+        fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+360)*10);  
+    else
+        fprintf(fid,'$ KBD_POS,%.0f,%10.0f,%9.0f,0,%4.0f,0,*0\n',A.X(1).Data(i)*1000,A.Y(970).Data(i)*1e7,A.Y(969).Data(i)*1e7,(A.Y(967).Data(i)+0)*10);  
+    end
+end
